254107325d697492d92c2ee89cdfc1197d844e46
jiayuanyuan <jiayuanyuan11260516@163.com>
2016-12-05 00:05:58 +0800

    updata

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@ var mySwiper_one = new Swiper('#box_top', {
 	speed: 700,
 	autoplayDisableOnInteraction: false,
 });
-function a(b){
-	var swiper=new Swiper(b,{
-	direction: 'horizontal',
-	autoplay: 5000,
-	speed: 700,
-	autoplayDisableOnInteraction: false,
-	nextButton: '.left-jt',
-	prevButton: '.right-jt'
+
+function a(b) {
+	var swiper = new Swiper(b, {
+		direction: 'horizontal',
+		autoplay: 5000,
+		speed: 700,
+		autoplayDisableOnInteraction: false,
+		nextButton: '.left-jt',
+		prevButton: '.right-jt'
 	})
 	return swiper;
 }
@@ -30,14 +31,36 @@ document.getElementById("videoSt").addEventListener('click', function() {
 	document.getElementById("videoSourse").play();
 }, false);
 $(function() {
-	$('.new_btn').click(function() {
-		$('.gsgs').css('display', 'block');
-		$('.gsywly').css('display', 'none');
-		a('#pageCar');
-	});
-	$('.next_btn').click(function(){
-		$('.gsgs').css('display', 'none');
-		$('.gsywly').css('display', 'block');
-		a('#pageCar1');
-	});
+$('.new_btn').click(function() {
+	$('.gsgs').css('display', 'block');
+	$('.gsywly').css('display', 'none');
+	a('#pageCar');
+});
+$('.next_btn').click(function() {
+	$('.gsgs').css('display', 'none');
+	$('.gsywly').css('display', 'block');
+	a('#pageCar1');
+});
+//var h=document.getElementById("cp2").getElementsByTagName("div").length;
+//	console.log(h)
+$('.lb div #guanbi').each(function(i) {
+	$(this).on('click',function(){
+		$(this).parent().hide();
+		$('.quanbu').show();
+	})	
+		
+	
+	
+	
+//	if(i == 3) 
+//	{
+//		$(this).css({
+//			'height': '30px',
+//			'background-color': 'yellow'
+//		});
+//	}
+})
+$('.quanbu').on('click',function(){
+	$('.lb div').show();
+})
 })
